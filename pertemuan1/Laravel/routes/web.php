5e7ad262c039c6f1cb7d07cdda3f1f0630073c0c
fahrizasalam <fahrizasallam@gmail.com>
2026-09-08 13:50:13 +0700

    pertemuan 2

--- pertemuan1/Laravel/routes/web.php
+++ pertemuan1/Laravel/routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mahasiswa/{nim}', function (string $nim) {
+    return 'Data mahasiswa dengan NIM ' . $nim;
+});
+
+Route::get('/matakuliah/{kode?}', function (?string $kode = null) {
+    if ($kode === null) {
+        return 'Menampilkan seluruh matakuliah';
+    }
+    return 'Menampilkan matakuliah kode ' . $kode;
+});
+
+Route::get('/semester/{angka}', function (int $angka) {
+    return 'Semester ke ' . $angka;
+})->whereNumber('angka');
+
+Route::get('/data-mahasiswa', [MahasiswaController::class,
+'index'])->name('mahasiswa.index');
+
+Route::get('/data-mahasiswa/{nim}', [MahasiswaController::class,
+'show'])->name('mahasiswa.show');
+
+Route::get('/cari-mahasiswa', [MahasiswaController::class, 'cari']);
